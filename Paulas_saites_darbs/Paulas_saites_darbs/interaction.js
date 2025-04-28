07adec8428d400182a3f2fff1db3fc0c7d6d52fb
Snuffii <129881727+Snuffii@users.noreply.github.com>
2025-04-28 23:10:39 +0300

    Delete Paulas_saites_darbs/Paulas_saites_darbs directory
    Deleted trash

--- Paulas_saites_darbs/Paulas_saites_darbs/interaction.js
+++
@@ -1,7 +0,0 @@
-const menu_slide = document.querySelector("#menu_slide");
-const showMenu = document.querySelector(".book");
-
-menu_slide.addEventListener("click", () => {
-  menu_slide.classList.toggle("active");
-  showMenu.classList.toggle("visible");
-});
